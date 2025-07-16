61539bd5e5f398a505397fa371523c5f0704f8bd
Movacs <kovacsmarcell1213@gmail.com>
2025-07-16 13:07:17 +0200

    Add todo item feature implemented.

--- Views/AddTodoPage.xaml.cs
+++ Views/AddTodoPage.xaml.cs
@@ -1,9 +1,15 @@
+using TodoAppMaui.ViewModels;
+
 namespace TodoAppMaui.Views;
 
 public partial class AddTodoPage : ContentPage
 {
-	public AddTodoPage()
+	private AddTodoViewModel _viewModel;
+	public AddTodoPage(AddTodoViewModel viewModel)
 	{
 		InitializeComponent();
+		BindingContext = viewModel;
+		_viewModel = viewModel;
 	}
+
 }
