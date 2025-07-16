f0eb3b8ba5c9fc46cee382095c8f10150b555b60
Movacs <kovacsmarcell1213@gmail.com>
2025-07-16 12:55:30 +0200

    Routing moved to code behind.

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,10 +1,14 @@
-﻿namespace TodoAppMaui
+﻿using TodoAppMaui.Views;
+
+namespace TodoAppMaui
 {
     public partial class AppShell : Shell
     {
         public AppShell()
         {
             InitializeComponent();
+            Routing.RegisterRoute(nameof(AddTodoPage), typeof(AddTodoPage));
+            Routing.RegisterRoute(nameof(TodoPage), typeof(TodoPage));
         }
     }
 }
