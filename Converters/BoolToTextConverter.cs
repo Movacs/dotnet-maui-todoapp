8176aee1625457523dfe3d86b46607dae7b70466
Movacs <kovacsmarcell1213@gmail.com>
2025-07-18 14:43:19 +0200

    Converters added.

---
+++ Converters/BoolToTextConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoAppMaui.Converters
+{
+    public class BoolToTextConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is bool b && b) ? "Not Done" : "Done";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return false;
+        }
+    }
+}
